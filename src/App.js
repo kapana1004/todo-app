f2399d3b89d655ec4615a10a118e00c76769b992
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2023-11-13 22:18:51 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import TodoClock from "./Components/TodoClock";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const weekday = time.toLocaleDateString(undefined, { weekday: "short" });
+  const monthDay = time.getDate();
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
+      setNewTask("");
+    }
+  };
+
+  const toggleTask = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" min-w-[100vw] min-h-[100vh] flex justify-center items-center flex-col ">
+      <h1 className=" text-[96px] text-blue-500">Todo</h1>
+      <div className=" relative">
+        <TodoClock
+          weekday={weekday}
+          monthDay={monthDay}
+          formattedTime={formattedTime}
+        />
+
+        <img
+          className=" w-[430px] "
+          src="/assets/cover-photo.png"
+          alt="flofers"
+        />
+
+        <div className=" flex flex-row justify-center pt-[17px] relative">
+          <input
+            className=" w-[275px] pl-[40px] rounded-[5px] bg-slate-200"
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Note"
+          />
+          <button
+            className=" bg-green-300  w-[88px] h-[49px] flex justify-center items-center ml-[10px] rounded-[5px]"
+            onClick={addTask}
+          >
+            <img src="/assets/Vector-plus.png" alt="plus" />
+          </button>
+          <div className=" w-[24px] h-[24px] rounded-[50px] bg-green-300 absolute left-[8%] top-[44%] flex justify-center items-center ">
+            <img src="/assets/Vector.png" alt="tic" />
+          </div>
+        </div>
+      </div>
+      <ul className=" mt-[20px]">
+        {tasks.map((task) => (
+          <li
+            className=" flex flex-row justify-between mb-[20px] "
+            key={task.id}
+          >
+            <p className=" mr-[140px] w-[149px] h-[22px] text-[18px] leading-[22px]">
+              {task.text}
+            </p>
+            <span> </span>
+            <div className=" w-[62px] flex justify-between items-center">
+              <div
+                className={`rounded-full ${
+                  task.completed ? "bg-green-300" : "bg-white"
+                } w-[20px] h-[20px] appearance-none border-[2px]  border-solid-[2px] border-green-300  flex justify-center items-center `}
+                onClick={() => toggleTask(task.id)}
+              >
+                <img
+                  className=" w-[9px] h-[7px]"
+                  src="/assets/Vector.png"
+                  alt="tic"
+                />
+              </div>
+
+              <img
+                className=" w-[24px] h-[24x]"
+                onClick={() => deleteTask(task.id)}
+                src="/assets/akar-icons_trash-can.png"
+                alt="delet"
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Components/TodoClock.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+export default function TodoClock({ formattedTime, monthDay, weekday }) {
+  return (
+    <div className=" w-[192px] absolute bottom-[30%] left-[70%]">
+      <p className=" text-[18px] font-bold text-white pl-[60px]">
+        {weekday} {monthDay}
+      </p>
+      <p className=" text-[48px] font-bold text-white">{formattedTime}</p>
+    </div>
+  );
+}
+
+// const [time, setTime] = useState(new Date());
+
+//   useEffect(() => {
+//     // Update the time every second
+//     const intervalId = setInterval(() => {
+//       setTime(new Date());
+//     }, 1000);
+
+//     // Clear the interval when the component is unmounted
+//     return () => clearInterval(intervalId);
+//   }, []); // Empty dependency array means the effect runs once after the initial render
+
+//   // Format the time as HH:mm:ss
+//   const formattedTime = time.toLocaleTimeString([], {
+//     hour: "2-digit",
+//     minute: "2-digit",
+//   });
+//   const weekday = time.toLocaleDateString(undefined, { weekday: "short" });
+//   const monthDay = time.getDate();
